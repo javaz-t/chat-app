caf91ab7f4d551059e2505450ec410f805769f8c
javaz-t <147338963+javaz-t@users.noreply.github.com>
2024-01-27 21:23:17 +0530

    second

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,9 @@ import 'package:chat_app/screen/auath.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-
+/// git working
 void main()async{
+  print('hekki');
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
