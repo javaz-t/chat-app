c49a59f5f8f85147a385383c1f53f8850c709522
javaz-t <147338963+javaz-t@users.noreply.github.com>
2024-01-30 03:06:13 +0530

    clear chat feature added

--- lib/screen/chat_screen.dart
+++ lib/screen/chat_screen.dart
@@ -1,5 +1,6 @@
 import 'package:chat_app/widgets/chat_messages.dart';
 import 'package:chat_app/widgets/new_mesages.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
@@ -18,8 +19,42 @@ class _ChatScreenState extends State<ChatScreen> {
     final token =await fcm.getToken();
     print('token == $token');// token can use ti test the notification
     fcm.subscribeToTopic('chat');
+ }
+ Future<void> clearChat() async {
 
-  }
+   showDialog(
+     context: context,
+     builder: (BuildContext context) {
+       return AlertDialog(
+         title: Text('Confirmation'),
+         content: Text('Are you sure you want to Clear Chat'),
+         actions: <Widget>[
+           TextButton(
+             child: Text('Cancel'),
+             onPressed: () {
+               Navigator.of(context).pop();
+             },
+           ),
+           TextButton(
+             child: Text('Yes'),
+             onPressed: () async{
+               print('clear Caht');
+               final collection = FirebaseFirestore.instance.collection('chat');
+               final docs = await collection.get();
+               for (var doc in docs.docs) {
+                 await doc.reference.delete();
+               }
+               Navigator.of(context).pop();
+             },
+           ),
+         ],
+       );
+     },
+   );
+
+
+
+ }
 
   @override
   void initState() {
@@ -34,6 +69,7 @@ class _ChatScreenState extends State<ChatScreen> {
       appBar: AppBar(
         title: Text('CHAT'),
         actions: [
+          IconButton(onPressed: clearChat, icon: Icon(Icons.delete)),
           IconButton(onPressed:(){
             FirebaseAuth.instance.signOut();
           }, icon: Icon(Icons.exit_to_app))
